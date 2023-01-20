2e1cf887860bbb7f1b984e03ad19dd6f42480c53
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2023-01-20 14:50:10 +0100

    objects overlap
    when 2 objects overlap, a line is printed to console

--- Assets/ObjSpawner.cs
+++ Assets/ObjSpawner.cs
@@ -3,21 +3,46 @@ using UnityEngine;
 public class ObjSpawner : MonoBehaviour
 {
     public GameObject objPrefab;
+    public GameObject newObjPrefab; // new object prefab to spawn when two objects overlap
     public float spawnInterval = 2f;
+    public float overlapCheckInterval = 0.5f; // interval to check for overlapping objects
+    public float overlapThreshold = 0.5f; // minimum overlap percentage for objects to be replaced
 
     private Vector2 spawnPosition;
     private int count = 0;
 
     void Start()
     {
-        spawnPosition = new Vector2(0f, 6);
         InvokeRepeating("SpawnObj", 2f, spawnInterval);
+        InvokeRepeating("CheckOverlap", overlapCheckInterval, overlapCheckInterval);
+
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Objects"), LayerMask.NameToLayer("Objects"));
     }
 
     void SpawnObj()
     {
-        GameObject instance = Instantiate(objPrefab, spawnPosition, Quaternion.identity);
+        float randomX = Random.Range(-2.5f, 2.5f);
+        Vector2 spawnPos = new Vector2(randomX, 6);
+        GameObject instance = Instantiate(objPrefab, spawnPos, Quaternion.identity);
         instance.name = "Object " + count;
         count++;
     }
+
+    void CheckOverlap()
+    {
+        GameObject[] allObjects = GameObject.FindGameObjectsWithTag("Object"); 
+        for (int i = 0; i < allObjects.Length; i++)
+        {
+            for (int j = i+1; j < allObjects.Length; j++)
+            {
+                if(allObjects[i].GetComponent<Collider2D>().bounds.Contains(allObjects[j].transform.position) || allObjects[j].GetComponent<Collider2D>().bounds.Contains(allObjects[i].transform.position))
+                {
+                    Debug.Log("overlap");
+                }
+            }
+        }
+    }
+
+
+    
 }
