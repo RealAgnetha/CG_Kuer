df6e3f57c0122bf13df9ff234450545fa8621ece
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2023-01-20 17:06:50 +0100

    Number of merges displayed onscreen

--- Assets/ObjSpawner.cs
+++ Assets/ObjSpawner.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using TMPro;
 
 public class ObjSpawner : MonoBehaviour
 {
     public GameObject objPrefab;
     public GameObject newObjPrefab; // new object prefab to spawn when two objects overlap
-  
+    public TextMeshProUGUI statusText;
+
     public float spawnInterval = 2f;
     public float overlapCheckInterval = 0.5f; // interval to check for overlapping objects
     public float overlapThreshold = 0.5f; // minimum overlap percentage for objects to be replaced
 
     private Vector2 spawnPosition;
     private int count = 0;
+    private int mergeCount = 0;
+
 
     void Start()
     {
@@ -18,6 +22,12 @@ public class ObjSpawner : MonoBehaviour
         InvokeRepeating("CheckOverlap", overlapCheckInterval, overlapCheckInterval);
 
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Objects"), LayerMask.NameToLayer("Objects"));
+        statusText.text = "Count: " + mergeCount;
+    }
+
+    void Update()
+    {
+        statusText.text = "Count: " + mergeCount;
     }
 
     void SpawnObj()
@@ -31,25 +41,25 @@ public class ObjSpawner : MonoBehaviour
 
     void CheckOverlap()
     {
-        GameObject[] allObjects = GameObject.FindGameObjectsWithTag("Object"); 
+        GameObject[] allObjects = GameObject.FindGameObjectsWithTag("Object");
         for (int i = 0; i < allObjects.Length; i++)
         {
-            for (int j = i+1; j < allObjects.Length; j++)
+            for (int j = i + 1; j < allObjects.Length; j++)
             {
-                if(allObjects[i].GetComponent<Collider2D>().bounds.Contains(allObjects[j].transform.position) || allObjects[j].GetComponent<Collider2D>().bounds.Contains(allObjects[i].transform.position))
+                if (allObjects[i].GetComponent<Collider2D>().bounds.Contains(allObjects[j].transform.position) ||
+                    allObjects[j].GetComponent<Collider2D>().bounds.Contains(allObjects[i].transform.position))
                 {
-                    Vector2 spawnPos = new Vector2((allObjects[i].transform.position.x + allObjects[j].transform.position.x) / 2, (allObjects[i].transform.position.y + allObjects[j].transform.position.y) / 2);
+                    Vector2 spawnPos =
+                        new Vector2((allObjects[i].transform.position.x + allObjects[j].transform.position.x) / 2,
+                            (allObjects[i].transform.position.y + allObjects[j].transform.position.y) / 2);
                     GameObject newInstance = Instantiate(newObjPrefab, spawnPos, Quaternion.identity);
                     newInstance.name = "New Object " + count;
                     count++;
                     Destroy(allObjects[i]);
                     Destroy(allObjects[j]);
+                    mergeCount++;
                 }
-
             }
         }
     }
-
-
-    
 }
