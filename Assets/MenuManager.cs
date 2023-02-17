fb6bf1285f436d3dccb784b493a923835496ca9b
Carolina <45121184+RealAgnetha@users.noreply.github.com>
2023-02-17 11:52:55 +0100

    add exit game functionality

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -3,10 +3,13 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-    
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
